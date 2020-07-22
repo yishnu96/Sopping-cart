2a530446de5d5f440de75e52c5c7d17472c5201e
yishnu96 <yishnupramanik96@gmail.com>
2020-07-22 15:11:02 +0530

    Added Icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import CartItem from './cart_item';
 function App() {
   return (
     <div className="App">
       <h1>Cart</h1>
+      <div>
+        <CartItem/>
+      </div>
     </div>
   );
 }
